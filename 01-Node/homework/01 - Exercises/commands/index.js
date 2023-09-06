2de6c7545cc5dd68769488d4360c93cd45dad25f
RobinsonG <hh.robinson95@hotmail.com>
2023-09-06 01:00:19 -0500

    Node done

--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,51 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd());
+};
 
-function date() {}
+function date(print) {
+    print(Date());
+};
 
-function echo() {}
+function echo(print, args) {
+    print(args);
+};
 
-function ls() {}
+function ls(print) {
+    fs.readdir('.', (error, files) => {
+        if (error) throw Error('error');
+        print(files.join('\n'));
+    });
+};
 
-function cat() {}
+const printFile = (print, fileName, lines) => {
+    fs.readFile(fileName, 'utf-8', (error, data) => {
+        if (error) throw Error('error');
+        lines === 'cat' && print(data);
+        lines === 'head' && print(data.split('\n').slice(0, 8).join('\n'));
+        lines === 'tail' && print(data.split('\n').at(-1));
+    });
+};
 
-function head() {}
+function cat(print, args) {
+    printFile(print, args, 'cat');
+};
 
-function tail() {}
+function head(print, args) {
+    printFile(print, args, 'head');
+};
 
-function curl() {}
+function tail(print, args) {
+    printFile(print, args, 'tail');
+};
 
-module.exports = {};
+function curl(print, args) {
+    utils.request(`https://${args}`, (error, response) => {
+        if (error) throw Error('error');
+        print(response);
+    });
+};
+
+module.exports = { pwd, date, echo, ls, cat, head, tail, curl };
--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -2,9 +2,16 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-module.exports =
+module.exports = http.createServer((request, response)=>{
+  console.log(`Server raised in port ${PORT}`);
+  if (request.url === '/api') {
+    fs.readFile("../utils/dogsData.json", (error, data)=>{
+      error ? response.writeHead(404,{'Content-Type':'text/plain'}),response.end('json not found') : this.address
+    });
+  }
+}).listen(PORT, 'localhost')
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
   null;
